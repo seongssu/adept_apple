73a9e7acf8b45f02367d48ecb9dcbe554a17651f
seongssu <loveforggong@gmail.com>
2023-08-23 15:30:33 +0900

    [PACK] : 🎁 ignore설정, 더미데이터, 샘플이미지

---
+++ app/src/main/java/com/android/apple/Item.kt
@@ -0,0 +1,12 @@
+package com.android.apple
+
+data class Item(
+    val Icon: Int,
+    val name: String,
+    val content: String,
+    val seller: String,
+    val price: String,
+    val adress: String,
+    val love: Int,
+    val chat: Int
+) {}
