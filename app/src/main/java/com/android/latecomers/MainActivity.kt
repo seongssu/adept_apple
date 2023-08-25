ee4146a2ec44231e86694952987edf2e88eefbee
seongssu <loveforggong@gmail.com>
2023-08-25 11:37:17 +0900

    [FEAT] : ⚙️ 탭뷰로 main&favorites 프래그먼트 나눔

--- app/src/main/java/com/android/latecomers/MainActivity.kt
+++ app/src/main/java/com/android/latecomers/MainActivity.kt
@@ -1,59 +1,69 @@
 package com.android.latecomers
 
+
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import com.android.latecomers.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var informationLauncher:ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
-        val modelList = mutableListOf<MemberData>()
-        modelList.add(MemberData(R.drawable.character1,"권은비","010-1111-1111"))
-        modelList.add(MemberData(R.drawable.character2,"강혜원","010-2222-2222"))
-        modelList.add(MemberData(R.drawable.character3,"최예나","010-3333-3333"))
-        modelList.add(MemberData(R.drawable.character4,"이채연","010-4444-4444"))
-        modelList.add(MemberData(R.drawable.character5,"김채원","010-5555-5555"))
-        modelList.add(MemberData(R.drawable.character6,"김민주","010-6666-6666"))
-        modelList.add(MemberData(R.drawable.character7,"조유리","010-7777-7777"))
-        modelList.add(MemberData(R.drawable.character8,"안유진","010-8888-8888"))
-        modelList.add(MemberData(R.drawable.character9,"장원영","010-9999-9999"))
-        modelList.add(MemberData(R.drawable.character10,"사쿠라","010-1234-5678"))
+        val tabLayout = binding.tabLayout
+        val viewPager = binding.viewPager
 
-        binding.recyclerview.adapter = MainActivityAdapter(modelList)
-        val adapter = MainActivityAdapter(modelList)
-        binding.recyclerview.adapter = adapter
-        binding.recyclerview.layoutManager = LinearLayoutManager(this)
+        setFragment(MainFragment())
 
-        val divider = DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
-        binding.recyclerview.addItemDecoration(divider)
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                val position = tab.position
 
-        adapter.memberClick = object : MainActivityAdapter.MemberClick{
-            override fun onClick(view: View, position:Int){
-                val item = modelList[position]
+                when (position) {
+                    0 -> {
+                        setFragment(MainFragment())
+                    }
 
-                //전화번호 눌렀을때 전화걸기
-                val phoneNumber = item.tel
-                val intent = Intent(Intent.ACTION_CALL)
-                intent.data = Uri.parse("tel:$phoneNumber")
-                try {
-                    startActivity(intent)
-                } catch(e:Exception){
-                    Snackbar.make(view, "전화 권한이 없습니다.", Snackbar.LENGTH_SHORT).show()
+                    1 -> {
+                        setFragment(FavoritesFragment())
+                    }
+                    // ... 그 외 탭 처리 ...
                 }
+            }
 
-                //즐겨찾기
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+            }
 
+            override fun onTabReselected(tab: TabLayout.Tab) {
+            }
+        }
+        )
+        informationLauncher = registerForActivityResult(ActivityResultContracts
+            .StartActivityForResult()){
+            if(it.resultCode == RESULT_OK){
+                val data = it.data
+                if(data !=null){
+
+                }
             }
         }
     }
+    private fun setFragment(frag: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.fragmentView, frag)
+            setReorderingAllowed(true)
+            addToBackStack("")
+        }
+    }
 }
+
--- app/src/main/java/com/android/latecomers/MemberData.kt
+++ app/src/main/java/com/android/latecomers/MemberData.kt
@@ -1,4 +1,4 @@
 package com.android.latecomers
 
-data class MemberData(val profile:Int, val name:String, val tel:String) {
+data class MemberData(val profile:Int, val name:String, val tel:String,var isFavorite: Boolean = false) {
 }
