4ce7f938c5d81af18ee90523cb21204968dc69ec
seongssu <loveforggong@gmail.com>
2023-08-30 15:51:18 +0900

    [FEAT] : ⚙️ 메인페이지 시작 시 TitleFragment 실행

--- app/src/main/java/com/android/newsreaderapp/NewsItem.kt
+++ app/src/main/java/com/android/newsreaderapp/NewsItem.kt
@@ -1,3 +1,7 @@
 package com.android.newsreaderapp
 
-data class NewsItem (val title:String, val article:String)
+data class NewsItem (val title:String, val article:String) : conect
+
+interface conect{}
+
+data class title(val title:String) : conect
